afbb00b12ac612178590308ac607d068fb441a97
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-06-08 22:30:49 +0100

    add js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+const coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
@@ -12,3 +12,24 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+const john = document.getElementById("john");
+const burt = document.getElementById("burt");
+const janine = document.getElementById("janine");
+const anna = document.getElementById("anna");
+
+john.addEventListener("click", function() {
+  document.getElementById("final-user").innerHTML = "John";
+});
+
+burt.addEventListener("click", function() {
+  document.getElementById("final-user").innerHTML = "Burt";
+});
+
+janine.addEventListener("click", function() {
+  document.getElementById("final-user").innerHTML = "Janine";
+});
+
+anna.addEventListener("click", function() {
+  document.getElementById("final-user").innerHTML = "Anna";
+});
